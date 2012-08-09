0c495cce75287bd824e9d53c7d20674f489acd0d
Miron Cuperman <miron+code@google.com>
2012-08-09 12:06:09 -0700

    Turn off stat queries for now, turn on caching

--- app/helpers/programs_helper.rb
+++ app/helpers/programs_helper.rb
@@ -2,9 +2,9 @@ module ProgramsHelper
   # Set up a program and relevant stats for display
   def program_stats(program)
     stats = {}
-    stats[:sections_count] = Section.where(:program_id => program.id).count()
-    stats[:sections_done_count] = Section.joins(:controls).where(:program_id => program.id).count(:distinct => true)
-    stats[:sections_undone_count] = Section.where(:program_id => program.id).count() - stats[:sections_done_count]
+    stats[:sections_count] = 0 #Section.where(:program_id => program.id).count()
+    stats[:sections_done_count] = 0 #Section.joins(:controls).where(:program_id => program.id).count(:distinct => true)
+    stats[:sections_undone_count] = 0 #Section.where(:program_id => program.id).count() - stats[:sections_done_count]
     stats[:sections_na_count] = 0
 
     if stats[:sections_count] > 0
@@ -13,9 +13,9 @@ module ProgramsHelper
       stats[:sections_undone_percentage] = 100.0 * stats[:sections_undone_count] / stats[:sections_count]
     end
 
-    controls = Control.joins(:sections).where(Section.arel_table[:program_id].eq(program.id))
-    stats[:controls_count] = controls.count(:distinct => true)
-    stats[:controls_parented_count] = controls.where(Control.arel_table[:parent_id].not_eq(nil)).count(:distinct => true)
+    #controls = Control.joins(:sections).where(Section.arel_table[:program_id].eq(program.id))
+    stats[:controls_count] = 0 #controls.count(:distinct => true)
+    stats[:controls_parented_count] = 0 #controls.where(Control.arel_table[:parent_id].not_eq(nil)).count(:distinct => true)
 
     stats[:controls_complying_count] = stats[:controls_count] / 3
     stats[:controls_approved_count] = stats[:controls_count] / 2 - stats[:controls_complying_count]
